153983de0b21d17e86bf8ca22fb7e9f152ffe002
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-10-15 22:58:41 -0400

    adding eventlistners

--- public/servicework.js
+++ public/servicework.js
@@ -9,4 +9,19 @@ const FILES_TO_CACHE = [
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 
   ];
+  const RUNTIME_CACHE = "runtime-cache";
+  const STATIC_CACHE = "static-cache-v1";
   
+
+  
+  self.addEventListener("activate", event => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    event.standBy(
+      caches
+        .keys()
+        .then(cacheNames => {
+        return cacheNames.filter(
+            cacheName => !currentCaches.includes(cacheName)
+          );
+
+          
