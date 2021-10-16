ace63652685bf6f6e13bd9942ad632545b71e1ca
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-10-15 22:58:04 -0400

    creating servicewrk.js '

---
+++ public/servicework.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+    "/index.html",
+    "/",
+    "/index.js",
+    "/styles.css",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+
+  ];
+  
